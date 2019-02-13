599a97601d138801e23f4f624572476060afda22
Alicia Santiago <albrown07@mac.com>
2019-02-13 22:15:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(calls)
   calls.collect {|call| puts call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do 
 end
 
 def find_the_cheese# code an argument here
