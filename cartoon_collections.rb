3276d7441625b71018bec1b2c0e9aae66f4a94e7
Alicia Santiago <albrown07@mac.com>
2019-02-13 22:08:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,6 @@ def roll_call_dwarves(names_arr)
   names_arr.each_with_index {|name, index| puts "#{index + 1}. #{name}"}
 end
 
-
 def summon_captain_planet# code an argument here
   # Your code here
 end
