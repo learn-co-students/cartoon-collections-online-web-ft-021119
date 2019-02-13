e31db37e06d407533f51bd1bb522903cc8890fd8
Alicia Santiago <albrown07@mac.com>
2019-02-13 22:14:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(names_arr)
 end
 
 def summon_captain_planet(calls)
-  calls.collect {|call| puts calls.capitalize + "!"}
+  calls.collect {|call| puts call.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
