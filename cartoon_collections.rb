c8322a4c02ae7084a7f680ccb1278e2528fc9467
Alicia Santiago <albrown07@mac.com>
2019-02-13 22:10:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(names_arr)
   names_arr.each_with_index {|name, index| puts "#{index + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.collect {|call| puts calls.collect.cap
 end
 
 def long_planeteer_calls# code an argument here
